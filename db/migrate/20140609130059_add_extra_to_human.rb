faa745d964e9c4cccea173d7c85c6b4a065011bd
Giovanni Messina <gmgp.info@gmail.com>
2014-06-09 17:41:49 +0200

    boolean extra e laboratori

---
+++ db/migrate/20140609130059_add_extra_to_human.rb
@@ -0,0 +1,6 @@
+class AddOneteamToHuman < ActiveRecord::Migration
+  def change
+    add_column :humen, :extra, :boolean, default: false
+    add_column :humen, :lab,   :boolean, default: false
+  end
+end
