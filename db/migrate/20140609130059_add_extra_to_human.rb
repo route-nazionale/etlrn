02e4b38dd75316cfb5eaae7008d6506ec1825b63
gmgp <gmgp.info@gmail.com>
2014-06-09 20:31:16 +0200

    fix migration name

--- db/migrate/20140609130059_add_extra_to_human.rb
+++ db/migrate/20140609130059_add_extra_to_human.rb
@@ -1,4 +1,4 @@
-class AddOneteamToHuman < ActiveRecord::Migration
+class AddExtraToHuman < ActiveRecord::Migration
   def change
     add_column :humen, :extra, :boolean, default: false
     add_column :humen, :lab,   :boolean, default: false
